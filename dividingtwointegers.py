592431e64c1082053ccc85a41f0a8d0172f036b4
Himanshu Saini <sainihimanshu.1999@gmail.com>
2021-03-09 00:05:06 +0530

    leetcode practice

---
+++ dividingtwointegers.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        isMinus = ((dividend<0 and divisor>0) or (dividend>0 and divisor<0))
+        ret = 0
+        dividend,divisor = abs(dividend),abs(divisor)
+        c,sub=1,divisor
+        
+        while(dividend>=divisor):
+            if(dividend>=sub):
+                dividend-=sub
+                ret += c
+                sub = (sub<<1)
+                c = (c<<1)
+                
+            else:
+                sub = (sub>>1)
+                c = (c>>1)
+                
+        if isMinus:
+            ret = -ret
+        return min(max(-2147483648,ret),2147483647)
