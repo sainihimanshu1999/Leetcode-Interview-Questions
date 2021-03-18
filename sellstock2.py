65accb2a4c59115f5f007b3f3e5bcb223e44249f
Himanshu Saini <sainihimanshu.1999@gmail.com>
2021-03-18 12:37:24 +0530

    leetcode done for the day

---
+++ sellstock2.py
@@ -0,0 +1,16 @@
+'''
+in this question you can do multiple transaction but not at same time
+
+'''
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(len(prices)-1):
+            profit += max((prices[i+1]-prices[i]),0)
+        return profit
+        
